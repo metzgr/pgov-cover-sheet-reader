454f3f2d2b99b98a628911a27e13368ecf0b8bab
jasondamico <damicoj@union.edu>
2021-07-01 16:51:46 -0400

    Initialize cover sheet reading (#1)
    * Create function to iterate through document elements in the order they appear on document
    
    * Add function to read data from cover sheet
    
    * Enable unique handling of checkboxes in iterating over cover sheet
    
    * Add function to create a DataFrame from a list of cover sheets
    
    * Add debugging function to print a docx table

---
+++ src/cover_sheet_reading.py
@@ -0,0 +1,87 @@
+"""
+Functions realted to scraping the data from an incoming cover sheet.
+"""
+
+import utility
+
+from docx.text.paragraph import Paragraph
+import pandas as pd
+
+# Maps headers on cover sheet to columns in the data
+HEADER_MAP = {
+    "What is blocking you?": "blockers_description",
+    "Add your own tags": "tags",
+    "How can the White House help?": "help_white_house",
+    "How can other agencies help?": "help_other_agencies",
+    "How can Congress help?": "help_congress",
+    "How can industry help?": "help_industry",
+    "How can the third sector (non-profits and non-governmental organizations) help?": "help_third_sector",
+    "How can academia help?": "help_academia"
+}
+
+
+def read_cover_sheet(document):
+    """
+    Returns a dictionary containing the data collected from the passed word document
+    
+    :param document: A Document object holding a performance cover sheet from a .docx file
+    :return: A dictionary containing all of the relevant data scraped from the passed cover sheet document
+    """
+    data = {}
+    header = None
+
+    for block in utility.iter_block_items(document):
+        if isinstance(block, Paragraph):
+            if block.text in HEADER_MAP.keys():     # if the paragraph holds one of the headers used to indicate that text can be inputted from user
+                header = block.text
+        else:   # if block is Table object
+            if header:  # if table comes directly after a header
+                text_input = None
+                for row in block.rows: 
+                    for cell in row.cells:
+                        for paragraph in cell.paragraphs: 
+                            if text_input:  # allows for multiple paragraphs in input
+                                text_input = "{} {}".format(text_input, paragraph.text)
+                            else:
+                                text_input = paragraph.text
+                data[HEADER_MAP[header]] = text_input
+                header = None
+            else:
+                for row in block.rows:
+                    row_text = []
+                    for cell in row.cells:
+                        for paragraph in cell.paragraphs: 
+                            row_text.append(paragraph.text)     # collects all paragraphs in a given cell into a list
+
+                    # storing of checkboxes in data field - continues until all checkboxes are retrieved from cell
+                    while any([i in ["☒", "☐"] for i in row_text]):
+                        try:
+                            checked_index = row_text.index("☒")
+                        except:
+                            checked_index = -1
+
+                        try:
+                            unchecked_index = row_text.index("☐")
+                        except:
+                            unchecked_index = -1
+
+                        index = max(checked_index, unchecked_index)
+                        checkbox = row_text.pop(index)
+                        column_title = row_text.pop(index).lower().replace(" ", "_")    # converts to snake_case
+
+                        data[column_title] = 1 if checkbox == "☒" else 0   # stores a 1 if box is checked, else 0
+    
+    return data
+
+def process_cover_sheets(cover_sheets_list):
+    """
+    Creates a DataFrame object from a list of cover sheets
+    
+    :param cover_sheets_list: A list of Document objects, each of which was created from a separate .docx file containing a cover sheet
+    :return: A DataFrame object with each row representing a cover sheet.
+    """
+    data = []
+    for cover_sheet in cover_sheets_list:
+        data.append(read_cover_sheet(cover_sheet))
+        
+    return pd.DataFrame(data)
---
+++ src/utility.py
@@ -0,0 +1,40 @@
+"""
+Holds functions that have use across multiple sections of the project.
+"""
+
+from docx.document import Document as _Document
+from docx.oxml.text.paragraph import CT_P
+from docx.oxml.table import CT_Tbl
+from docx.table import _Cell, Table
+from docx.text.paragraph import Paragraph
+
+def iter_block_items(parent):
+    """
+    Returns a stream of Paragraph and Table objects in the order in which they appear in the passed docx document.
+
+    :param parent: A Document object holding a docx file.
+    :return: A stream of Paragraph and Table objects in the order in which they appear on the document passed as a parameter.
+    """
+    if isinstance(parent, _Document):
+        parent_elm = parent.element.body
+    elif isinstance(parent, _Cell):
+        parent_elm = parent._tc
+    else:
+        raise ValueError("Parent object is of unknown type")
+
+    for child in parent_elm.iterchildren():
+        if isinstance(child, CT_P):
+            yield Paragraph(child, parent)
+        elif isinstance(child, CT_Tbl):
+            yield Table(child, parent)
+
+def print_table(table):
+    """
+    Prints out each component of the passed Table object in the order in which it appears.
+
+    :param table: A Table object representing a table that holds text.
+    """
+    for row in table.rows: 
+        for cell in row.cells:
+            for paragraph in cell.paragraphs: 
+                print(paragraph.text)
