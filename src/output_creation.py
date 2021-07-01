4f09714b4c6b336847608a5087a91966251151cf
jasondamico <damicoj@union.edu>
2021-07-01 17:58:45 -0400

    Create system of retrieving keywords from paragraph text

---
+++ src/output_creation.py
@@ -0,0 +1,27 @@
+"""
+Maps keywords imbedded in template document (keys) to what they will be replaced with in the rendered output document (values). Note that each keyword is identified as "{{keyword_name}}" within the template document.
+"""
+
+import utility
+
+
+def get_keywords(paragraph):
+    """
+    Given a paragraph, returns a list of the keywords held within it. NOTE: Keywords are contained within {{double curly brackets}} in the template document.
+
+    :param paragraph: A string of text that may contain keywords contained within double curly brackets (ex: {{keyword}}).
+    :return: A list of the keywords contained within the passed text.
+    """
+    paragraph_copy = paragraph  # Preserves the state of the original text
+    keywords = []
+    
+    while "{{" in paragraph_copy and "}}" in paragraph_copy:
+        keyword = paragraph_copy[paragraph_copy.index("{{") + 2:paragraph_copy.index("}}")]
+        
+        # Removes keyword indicators from the text after the keyword has been stored
+        paragraph_copy = paragraph_copy.replace("{{", "", 1)
+        paragraph_copy = paragraph_copy.replace("}}", "", 1)
+
+        keywords.append(keyword)
+        
+    return keywords
