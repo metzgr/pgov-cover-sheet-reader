da6b92055e65b6cd1cb7f1574d495f8a85c7d7f5
jasondamico <damicoj@union.edu>
2021-07-06 09:46:47 -0400

    Create utility method for retrieving a DataFrame of goal statuses
    Method was designed to be versatile; can adapt to various specifications passed as arguments.

--- src/agency.py
+++ src/agency.py
@@ -68,6 +68,31 @@ class Agency():
         """
         return list(self.agency_df["Goal Name"].unique())
 
+    def get_goal_status_df(self, goal_names=None, year=None, quarter=None):
+        """
+        Returns a DataFrame mapping each agency APG name to its status for the specified year and quarter.
+
+        param goal_names: A list of the goal names to be represented in the returned DataFrame. Defaults to returning all goals within the represented agency.
+        :param year: The year from which to retrieve goal status. Defaults to the year that the object represents, "all" returns the data from all years and quarters.
+        :param quarter: The quarter from which to retrieve goal status. Defaults to the quarter that the object represents, "all" returns the data from all years and quarters.
+        :return: A DataFrame mapping each APG to its goal status across the specified year and quarters.
+        """
+        if not year:
+            year = self.current_year
+
+        if not quarter:
+            quarter = self.current_quarter
+
+        conditional = pd.Series(data=[True for i in range(len(self.agency_df))], index=self.agency_df.index)     # defaults to all rows
+        
+        if not "all" in [year, quarter]:
+            conditional = conditional & (self.agency_df["Fiscal Year"] == year) & (self.agency_df["Quarter"] == quarter)
+        
+        if goal_names:
+            conditional = conditional & (self.agency_df["Goal Name"].isin(goal_names))
+
+        return self.agency_df.loc[conditional, ["Goal Name", "Quarter", "Fiscal Year", "Status"]].reset_index(drop=True)
+
     def get_goal_status(self, goal_name, year=None, quarter=None):
         """
         Returns the goal status of the passed APG.
