2778603c3d9299583fbff4ae276b5a703deb9acc
jasondamico <damicoj@union.edu>
2021-07-06 09:43:15 -0400

    Create getter methods

--- src/agency.py
+++ src/agency.py
@@ -15,3 +15,45 @@ class Agency():
         self.abbreviation = ""  # the abbreviation of the passed agency
         self.current_quarter = current_quarter 
         self.current_year = current_year
+
+    # GETTER METHODS
+
+    def get_agency_df(self):
+        """
+        Returns the DataFrame representing the agency and its APGs.
+
+        :return: A slice of the central DataFrame used to initialize the object that only containts rows relevant to the represented agency.
+        """
+        return self.agency_df
+
+    def get_name(self):
+        """
+        Returns the name of the agency represented by the object.
+
+        :return: The name of the agency that the object represents.
+        """
+        return self.name
+    
+    def get_abbreviation(self):
+        """
+        Returns the most common abbreviation of the agency represented by the object.
+
+        :return: The most common abbreviation of the agency represented by the object.
+        """
+        return self.abbreviation
+
+    def get_quarter(self):
+        """
+        Returns the quarter that the object represents.
+
+        :return: The quarter that the Agency object represents.
+        """
+        return self.current_quarter
+
+    def get_year(self):
+        """
+        Returns the year that the object represents.
+
+        :return: The year that the Agency object represents.
+        """
+        return self.current_year
