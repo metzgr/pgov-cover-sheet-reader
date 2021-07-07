d3d1222dfe64646f786ac86f76e4601793daccbc
jasondamico <damicoj@union.edu>
2021-07-07 15:38:30 -0400

    Resolve runtime error with `matplotlib.pyplot` import
    The error I was getting was `OSError: no library called "cairo" was found`. After a lot of struggling to find anything helpful, [this StackOverflow post](https://stackoverflow.com/questions/44272733/cant-import-matplotlib-pyplot-as-plt) provided the solution.

--- src/viz.py
+++ src/viz.py
@@ -2,6 +2,8 @@
 Guides the creation of visualizations for the summary report from the source data.
 """
 
+import matplotlib
+matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
