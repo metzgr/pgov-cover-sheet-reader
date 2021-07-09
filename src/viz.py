11e90ff5cd11813f780e3683033b61da1cd98d37
jasondamico <damicoj@union.edu>
2021-07-09 18:41:26 -0400

    Create function that saves challenges area chart

--- src/viz.py
+++ src/viz.py
@@ -126,6 +126,42 @@ def create_challenges_reported_in_quarter(agency, dir=DEFAULT_DIRECTORY, name="c
     fig.set_size_inches(12, 8)   # saved image is larger, of higher quality
     __save_figure(fig, dir, name)
 
+def create_challenges_area_chart(agency, dir=DEFAULT_DIRECTORY, name="challenges_area_chart"):
+    """
+    Creates an area chart showing the cumulative amount of challenges reported in each category.
+
+    :param agency: The Agency object from which a challenges area chart will be created.
+    :param dir: The directory to which the figure will be saved to.
+    :param name: The file name that the figure will be saved to.
+    """
+    # Retrieve DataFrame, sort values in chronological order
+    challenge_count_df = df_creator.get_challenge_count_by_quarter(agency.get_agency_df())
+    challenge_count_df.sort_values(by=["Fiscal Year", "Quarter"])
+
+    data = {}
+
+    # Create data entry of cumulative sum for each challenge. Cumulative sum is in chronological order, dating from the furthest back quarter reported to the most recent
+    for challenge in utility.CHALLENGES_LIST:
+        cumsum = list(challenge_count_df.loc[challenge_count_df["Challenge"] == challenge]["Count"].cumsum())
+        data[challenge] = cumsum
+        
+    # Create area plot from cumulative sum data
+    pd.DataFrame(data).plot.area(stacked=False)
+
+    fig = plt.gcf()
+    ax = plt.gca()
+
+    # Editing the display of the plot
+    quarter_list = [f"{quarter}" for year in challenge_count_df["Fiscal Year"].unique() for quarter in challenge_count_df["Quarter"].unique()]  # a list of all quarters stored in DataFrame
+    plt.xticks([i for i in range(len(quarter_list))], quarter_list, fontsize=24)
+    plt.yticks(fontsize=24)
+    ax.grid(False)
+    plt.legend(prop={'size': 20})
+
+    # Exporting figure
+    fig.set_size_inches(12, 8)
+    __save_figure(plt.gcf(), dir, name)
+
 def __save_figure(fig, dir, name):
     """
     Saves the passed figure to the passed directory path and name.
