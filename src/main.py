028cd4780af7c2d65517ee905af03972975cf111
jasondamico <damicoj@union.edu>
2021-07-12 10:47:54 -0400

    Wrap all executed code in if __name__ == "__main__" check

--- src/main.py
+++ src/main.py
@@ -2,5 +2,6 @@ import output_creation
 import agency
 import pandas as pd
 
-sba = agency.Agency(pd.read_csv("../dummy_cover_sheet_data.csv"), "SBA", "Q4", 2020)
-output_creation.create_summary_document("../Summary_Report_Template.docx", sba)
+if __name__ == "__main__":
+    sba = agency.Agency(pd.read_csv("../dummy_cover_sheet_data.csv"), "SBA", "Q4", 2020)    # created for debugging in development
+    output_creation.create_summary_document("../Summary_Report_Template.docx", sba)
