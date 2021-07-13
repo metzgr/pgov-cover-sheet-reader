a34f8fd55171579a10379969a8e1d467ded72e2f
jasondamico <damicoj@union.edu>
2021-07-13 15:04:15 -0400

    Add page breaks following each APG breakdown page

--- src/output_creation.py
+++ src/output_creation.py
@@ -98,6 +98,10 @@ def create_summary_document(template_path, agency, output_dir="../"):
 
         apg_template.render(context)    # renders the keyword replacements specific to the APG
 
+        # Adds page break after every APG breakdown except for on final page
+        if i != len(apgs_list) - 1:
+            apg_template.add_page_break()
+
         # Loops through every element in the APG summary, adds it to the whole agency summary report
         for element in apg_template.element.body:
             tpl.docx.element.body.append(element)
