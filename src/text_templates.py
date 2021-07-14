a52bdb26923c2ff933d0cdfec1f11d8cd9abb99d
jasondamico <damicoj@union.edu>
2021-07-14 17:00:51 -0400

    Add APG blockers field to APG breakdown page (#26)
    * Initialize `get_blockers_text()`
    
    * Return input from cover sheet identifying blockers for APG
    
    * Implement `get_blockers_text()` into dynamic generation of summary report

--- src/text_templates.py
+++ src/text_templates.py
@@ -149,6 +149,20 @@ def get_speedometer_summary_text(agency, apg_name):
 
     return rt
 
+def get_blockers_text(agency, apg_name):
+    """
+    Returns a string describing the blockers identified for the passed agency and APG. String returned is raw input given in cover sheet.
+
+    :param agency: An Agency object representing a CFO Act agency at a given point in time.
+    :param apg_name: The name of the APG whose status will be summarized.
+    :return: A string describing the blockers identified for the passed agency and APG. String returned is raw input given in cover sheet.
+    """
+    # Obtaining the row that represents the APG in the reporting quarter/fiscal year
+    agency_df = agency.get_agency_df()
+    apg_row = agency_df.loc[(agency_df["Goal Name"] == apg_name) & (agency_df["Quarter"] == agency.get_quarter()) & (agency_df["Fiscal Year"] == agency.get_year())]
+
+    return apg_row["Blockers"].values[0]
+
 def get_group_help_text(agency, apg_name):
     """
     Returns a RichText object describing the help that the passed APG's goal team has requested from various sectors.
