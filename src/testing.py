f9f863b0e2f187e1754213f71552133c8cd723dc
jasondamico <damicoj@union.edu>
2021-07-16 09:09:49 -0400

    Update calls of `create_summary_document()` with output filename

--- src/testing.py
+++ src/testing.py
@@ -7,4 +7,4 @@ import pandas as pd
 
 if __name__ == "__main__":
     sba = agency.Agency(pd.read_csv("../dummy_cover_sheet_data.csv"), "SBA", "Q4", 2020)
-    output_creation.create_summary_document(sba)
+    output_creation.create_summary_document(sba, "SBA_output")
