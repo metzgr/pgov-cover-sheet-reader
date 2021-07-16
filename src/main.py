639ab597d2f5346f9d8a42fbeed7116004c69f06
jasondamico <damicoj@union.edu>
2021-07-16 10:14:17 -0400

    Add loop to create summary document for every agency abbreviation

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@
 File to be run to generate summary reports for the most recent quarter
 """
 import output_creation
-import agency
+from agency import Agency
 import pandas as pd
 
+from constants import AGENCY_ABBREVIATION_TO_NAME
+
 if __name__ == "__main__":
-    pass
+    for agency_abbreviation in AGENCY_ABBREVIATION_TO_NAME.keys():
+        file_name = f"{agency_abbreviation}_Summary"
+        agency = Agency(pd.read_csv("../dummy_cover_sheet_data.csv"), agency_abbreviation, "Q4", 2020)
+        output_creation.create_summary_document(agency, file_name)
+        print(file_name, "created")
