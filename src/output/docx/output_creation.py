a43591c81ad15673e88183538c150ab0c7db27e9
jasondamico <damicoj@union.edu>
2021-07-20 11:48:35 -0400

    Move table creation to its own function

--- src/output/docx/output_creation.py
+++ src/output/docx/output_creation.py
@@ -74,20 +74,6 @@ def create_summary_document(agency, output_filename, output_dir="src/output/docx
 
     recurring_challenges_df = get_top_recurring_challenges(agency)
 
-    table = []
-
-    previous_quarter, previous_year = utility.get_previous_quarter_and_year(agency.get_quarter(), agency.get_year())
-
-    for goal in agency.get_goals():
-        row = [goal]
-
-        for quarter, year in zip([previous_quarter, agency.get_quarter()], [previous_year, agency.get_year()]):
-            status = agency.get_goal_status(goal, quarter=quarter, year=year)
-
-            row.append(status)
-
-        table.append({"cols": row})
-
     replacement_map = {
         "previous_quarter_and_year": "{} {}".format(*utility.get_previous_quarter_and_year(agency.get_quarter(), agency.get_year())),
         "current_quarter_and_year": f"{agency.get_quarter()} {agency.get_year()}",
@@ -103,7 +89,7 @@ def create_summary_document(agency, output_filename, output_dir="src/output/docx
         "recur_challenge_2_goal": recurring_challenges_df.iloc[1]["Goal Name"],
         "challenge_summary_text": text_templates.get_challenge_summary_text(agency),
         'col_labels' : ['fruit', 'vegetable', 'stone', 'thing'],
-        'tbl_contents': table
+        'tbl_contents': get_goal_status_table(agency)
     }
 
     tpl.render(replacement_map)
@@ -174,3 +160,28 @@ def get_top_recurring_challenges(agency, num_challenges=2):
     df = df.sort_values("Count", ascending=False)
 
     return df.reset_index(drop=True).head(num_challenges)
+
+def get_goal_status_table(agency):
+    """
+    Returns a list with nested dictionaries (representing rows of the the table) to fill the goal status table in the template document.
+
+    :param agency: An Agency object representing the agency for which the top recurring challenges will be retrieved.
+    :return: A DataFrame with the most frequent recurring challenges for the passed agency.
+    """
+    table = []
+    previous_quarter, previous_year = utility.get_previous_quarter_and_year(agency.get_quarter(), agency.get_year())
+    quarters = [previous_quarter, agency.get_quarter()]
+    years = [previous_year, agency.get_year()]
+
+    for goal in agency.get_goals():
+        row = [goal]    # first column is filled with the goal name
+
+        # Loops first through the previous quarter/year, then the current quarter/year
+        for quarter, year in zip(quarters, years):
+            status = agency.get_goal_status(goal, quarter=quarter, year=year)
+
+            row.append(status)  # appends goal status as a new column
+
+        table.append({"cols": row})     # appends row to the table object
+
+    return table
