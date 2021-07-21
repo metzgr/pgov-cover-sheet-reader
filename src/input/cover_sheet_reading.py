0007f82ff17586bd1fedb179895e4505cfecc1e9
jasondamico <damicoj@union.edu>
2021-07-21 10:43:03 -0400

    Correct utility import

--- src/input/cover_sheet_reading.py
+++ src/input/cover_sheet_reading.py
@@ -2,7 +2,7 @@
 Functions realted to scraping the data from an incoming cover sheet.
 """
 
-import src.utility
+import src.utility as utility
 
 from docx.text.paragraph import Paragraph
 import pandas as pd
