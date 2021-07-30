fe2e4459e83ffd744e905e45f7d7a755a5746dc3
jasondamico <damicoj@union.edu>
2021-07-30 15:42:48 -0400

    Add Markdown processing utility functions

--- src/output/text/processing/markdown.py
+++ src/output/text/processing/markdown.py
@@ -5,3 +5,19 @@ from src.constants import BOLD_REGEX, ITALICS_REGEX
 
 from docxtpl import RichText
 import re
+
+def __get_bold_items(text):
+    """
+    Returns a list of all the portions of the passed string that are desired to be bolded according to Markdown formatting standards (**between two sets of double asterisks**).
+
+    :return: A list of items of the passed string to be bolded according to Markdown formatting standards.
+    """
+    return list(filter(None, re.findall(BOLD_REGEX, text)))
+
+def __get_italics_items(text):
+    """
+    Returns a list of all the portions of the passed string that are desired to be italicized according to Markdown formatting standards (*between two sets of asterisks*).
+
+    :return: A list of items of the passed string to be italicized according to Markdown formatting standards.
+    """
+    return list(filter(None, re.findall(ITALICS_REGEX, text)))
