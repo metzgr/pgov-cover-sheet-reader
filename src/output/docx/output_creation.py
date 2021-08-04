2c19bff2dbdcb86c5eebcd76e358236165ef2776
jasondamico <damicoj@union.edu>
2021-08-04 09:42:59 -0400

    Convert recommendations to table, remove list of challenges (#86)
    * Change `get_recommendations_for_challenge()` to return a DataFrame
    
    DataFrame is more applicable to generating a table than a list of dictionaries, is easier to work with.
    
    * Remove challenge bullets from APG summary, add recommendations table
    
    * Dynamically fill recommendations table
    
    * Change rendered recs to single RichText object
    
    This allows for greater control of the formatting of the the recommendations as a whole.
    
    * Add comments to recs table function
    
    * Move rec text block retrieval to `text_templates.py`
    
    * Increase margins of recommendations table
    
    * Move resetting of index for recommendations df to function definition
    
    This means that any DataFrame returned will not appear as a slice of the larger spreadsheet of recommendations for challenges, but rather as a unique DataFrame of recommendations, as desired.

--- src/output/docx/output_creation.py
+++ src/output/docx/output_creation.py
@@ -7,13 +7,14 @@ import src.objects.agency as agency
 import src.output.text.text_templates as text_templates
 import src.output.data.df_creator as df_creator
 import src.output.viz.viz as viz
+from src.output.text.processing.excel import get_recommendations_for_challenge
 from src.constants import VIZ_DIRECTORY, SUMMARY_TEMPLATE_PATH, APG_BREAKDOWN_TEMPLATE_PATH
 
 import os
 from docx.shared import Inches
 from docx.enum.text import WD_BREAK
 from docx.text.paragraph import Paragraph
-from docxtpl import DocxTemplate, InlineImage
+from docxtpl import DocxTemplate, InlineImage, RichText
 import pandas as pd
 
 # Maps keywords within the template document to the values that they will be replaced by.
@@ -121,8 +122,8 @@ def create_summary_document(agency, output_filename, output_dir="src/output/docx
             "speedometer_text": text_templates.get_speedometer_summary_text(agency, apg),
             "blockers_text": text_templates.get_blockers_text(agency, apg),
             "group_assistance_text": text_templates.get_group_help_text(agency, apg),
-            "challenge_bullets": text_templates.get_apg_challenges_bullets(agency, apg, tpl),
-            "success_story": text_templates.get_success_story(agency, apg)
+            "success_story": text_templates.get_success_story(agency, apg),
+            "recs_table": get_recs_table(agency, apg, tpl)
         }
 
         # Fill placeholders of image tags
@@ -220,6 +221,41 @@ def get_challenge_count_table(agency):
                 "count": count
             }
         })
+
+    return table
+
+def get_recs_table(agency, goal_name, tpl):
+    """
+    Returns a list of dictionaries (representing rows of the table) to fill the recommendations table in the APG breakdown template.
+
+    :param agency: An Agency object representing the agency for which challenge mitigation recommendations will be made.
+    :param goal_name: The goal from which suggestions will be made based on their challenges.
+    :return: A list of dictionaries used to render the recommendations table.
+    """
+    table = []
+
+    # Adds row based on every challenge the agency is facing in the current quarter
+    for challenge in agency.get_challenges(goal_name):
+        recs_df = get_recommendations_for_challenge(challenge)
+
+        rt = RichText()
+
+        for index, row in recs_df.iterrows():   # NOTE: this is not implemented in the text template spreadsheet because the hyperlink needs to be added to the recommendations
+            rec = row["Recommended Action"]
+            explanation = row['Explanation']
+            url = tpl.build_url_id(row["URL"])
+
+            rt.add(text_templates.get_rec_text_block(rec, explanation, url))
+            
+            if index != len(recs_df) - 1:   # add line breaks between all recommendations except for the last
+                rt.add("\n\n")
+        
+        table_row = {
+            "challenge": challenge,
+            "recs": rt
+        }
+
+        table.append(table_row)
 
     return table
 
