6809e8a92f1047a9f9c5972fbbb625cc0f5c320f
jasondamico <damicoj@union.edu>
2021-08-04 15:27:19 -0400

    Group constants by related functions
    Moved multi-line comments to single-line comments as a convention; using multi-line comments for groups, single-line comments for describing individual constants

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,33 @@
 import pandas as pd
 
 """
-A dictionary mapping each goal status to a rank
+PATHS
 """
+SUMMARY_TEMPLATE_PATH = "src/resources/templates/docx/Summary_Report_Template.docx"
+APG_BREAKDOWN_TEMPLATE_PATH = "src/resources/templates/docx/APG_Summary_Template.docx"
+TEXT_BLOCK_TEMPLATES_PATH = "src/resources/templates/excel/text_block_templates.xlsx"
+CHALLENGES_RECOMMENDATIONS_MAP_PATH = "src/resources/templates/excel/challenges_recommendations_map.xlsx"
+
+"""
+DIRECTORIES
+"""
+VIZ_DIRECTORY = "src/output/viz/images/"
+
+# A path to the directory in which cover sheets are stored (relative to the location of the project's root).
+COVER_SHEET_DIRECTORY = "../cover_sheet/cover_sheets/"
+
+"""
+COLUMN NAME LISTS
+"""
+CHALLENGES_LIST = ["Hiring technical staff", "Inadequate technical solutions", "Challenge 3", "Challenge 4", "Challenge 5", "Challenge 6"]
+
+# List containing administration goals, which are also used as column names.
+THEMES_LIST = ["Climate", "Equity", "Recovery"]
+
+"""
+GOAL STATUSES
+"""
+# A dictionary mapping each goal status to a rank
 STATUS_RANK_MAP = {
     "Ahead": 3,
     "On track": 2,
@@ -11,8 +36,9 @@ STATUS_RANK_MAP = {
 }
 
 """
-Maps each goal status to a hex color
+STYLING
 """
+# Maps each goal status to a hex color
 STATUS_COLOR_MAP = {
     "Ahead": "#156966",
     "On track": "#319C98",
@@ -21,19 +47,8 @@ STATUS_COLOR_MAP = {
 }
 
 """
-Paths for various templates needed to dynamically generate the summary report.
+AGENCY NAMES/ABBREVIATIONS
 """
-SUMMARY_TEMPLATE_PATH = "src/resources/templates/docx/Summary_Report_Template.docx"
-APG_BREAKDOWN_TEMPLATE_PATH = "src/resources/templates/docx/APG_Summary_Template.docx"
-TEXT_BLOCK_TEMPLATES_PATH = "src/resources/templates/excel/text_block_templates.xlsx"
-CHALLENGES_RECOMMENDATIONS_MAP_PATH = "src/resources/templates/excel/challenges_recommendations_map.xlsx"
-
-"""
-Constants related to the challenges expressed by APG teams
-"""
-CHALLENGES_RECOMMENDATIONS_MAP_DF = pd.read_excel(CHALLENGES_RECOMMENDATIONS_MAP_PATH)
-CHALLENGES_LIST = ["Hiring technical staff", "Inadequate technical solutions", "Challenge 3", "Challenge 4", "Challenge 5", "Challenge 6"]
-
 AGENCY_NAME_TO_ABBREVIATION = {
     "Department of Agriculture": "USDA", 
     "Department of Commerce": "DOC",
@@ -62,22 +77,15 @@ AGENCY_NAME_TO_ABBREVIATION = {
 
 AGENCY_ABBREVIATION_TO_NAME = {value: key for key, value in AGENCY_NAME_TO_ABBREVIATION.items()}
 
-VIZ_DIRECTORY = "src/output/viz/images/"
-
 """
-A path to the directory in which cover sheets are stored (relative to the location of the project's root).
-"""
-COVER_SHEET_DIRECTORY = "../cover_sheet/cover_sheets/"
-
-"""
-Text block template constants
+MARKDOWN REGEX
 """
 BOLD_REGEX = "\*\*(.*?)\*\*"
 ITALICS_REGEX = "\*(.*?)\*"
 BOLD_ITALICS_REGEX = ""
-TEXT_BLOCK_TEMPLATES_DF = pd.read_excel(TEXT_BLOCK_TEMPLATES_PATH, skiprows=1)
 
 """
-List containing administration goals, which are also used as column names.
+NO-CODE SPREADSHEETS
 """
-THEMES_LIST = ["Climate", "Equity", "Recovery"]
+TEXT_BLOCK_TEMPLATES_DF = pd.read_excel(TEXT_BLOCK_TEMPLATES_PATH, skiprows=1)
+CHALLENGES_RECOMMENDATIONS_MAP_DF = pd.read_excel(CHALLENGES_RECOMMENDATIONS_MAP_PATH)
