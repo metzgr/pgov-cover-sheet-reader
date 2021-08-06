35894115e92c4bef9b2de1b005c4dfa72a0655b7
jasondamico <damicoj@union.edu>
2021-08-06 16:00:28 -0400

    Add comments for future implementation

--- src/input/upload.py
+++ src/input/upload.py
@@ -11,6 +11,8 @@ def update_database(database_path, new_data_df):
     :param database_path: The path to the central data storage for the project. Only supports .csv files (could be expanded in the future).
     :param new_data_df: A DataFrame holding data to be added to the database, presumably read from cover sheets.
     """
+    # NOTE: In future refinements of this function, there is an opportunity to reject input/raise an error here if certain columns are missing (e.g., Quarter, Fiscal Year and Agency Name).
+
     database = pd.read_csv(database_path)
     different_columns = new_data_df.columns.difference(database.columns).to_list()  # list of columns in the cover sheets but not in database
 
