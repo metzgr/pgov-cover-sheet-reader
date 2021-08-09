a4e9582c60363a54f4b23b18ecb03878505c6e67
jasondamico <damicoj@union.edu>
2021-08-09 14:29:16 -0400

    Fix incorrect import of constants in `testing.py`

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ import src.output.docx.output_creation as output_creation
 from src.objects.agency import Agency
 import pandas as pd
 
-from constants import DATABASE_PATH
+from src.constants import DATABASE_PATH
 
 if __name__ == "__main__":
     sba = Agency(pd.read_csv(DATABASE_PATH), "SBA", "Q4", 2020)
